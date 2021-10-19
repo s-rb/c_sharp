e3c78f468085660a01c136ddc677df0ea8ed0985
Roman Surkov <s.rb@list.ru>
2021-10-19 18:09:59 +0200

    Hello world console app

---
+++ 1/HelloWorldConsoleApp/HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        /// <summary>
+        /// Точка входа
+        /// </summary>
+        /// <param name="args">Параметры запуска</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        }
+    }
+}
