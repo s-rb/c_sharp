cb704e8a8db162e5bf651aa560be3cdbdd1e431b
Roman Surkov <s.rb@list.ru>
2021-10-21 12:13:24 +0200

    centered sysout modified

--- 2.6 Домашнее Задание/CopyBook/CopyBook/Program.cs
+++ 2.6 Домашнее Задание/CopyBook/CopyBook/Program.cs
@@ -9,6 +9,10 @@ namespace CopyBook
 {
     class Program
     {
+        /// <summary>
+        /// Основной метод
+        /// </summary>
+        /// <param name="args">Параметры запуска</param>
         static void Main(string[] args)
         {
             string name;
@@ -38,19 +42,34 @@ namespace CopyBook
             rusScore = int.Parse(ReadLine());
 
             averageScore = GetAverage((double)historyScore, (double)mathScore, (double)rusScore);
-
-
             Print(name, age, height, historyScore, mathScore, rusScore, averageScore);
             PrintFormatted(name, age, height, historyScore, mathScore, rusScore, averageScore);
             PrintInterpolated(name, age, height, historyScore, mathScore, rusScore, averageScore);
             PrintInterpolatedCentered(name, age, height, historyScore, mathScore, rusScore, averageScore);
         }
 
+        /// <summary>
+        /// Метод подсчета среднего балла
+        /// </summary>
+        /// <param name="score1">Балл1</param>
+        /// <param name="score2">Балл2</param>
+        /// <param name="score3">Балл3</param>
+        /// <returns>Среднее количество баллов</returns>
         static double GetAverage(double score1, double score2, double score3)
         {
             return (score1 + score2 + score3) / 3;
         }
 
+        /// <summary>
+        /// Метод обычно печати
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="height">Рост</param>
+        /// <param name="historyScore">Баллы по истории</param>
+        /// <param name="mathScore">Баллы по математике</param>
+        /// <param name="rusScore">Баллы по русскому языку</param>
+        /// <param name="averageScore">Средний балл</param>
         static void Print(string name, int age, int height, int historyScore, int mathScore, int rusScore, double averageScore)
         {
             WriteLine("\n------------------- Обычный вывод ---------------------");
@@ -58,6 +77,16 @@ namespace CopyBook
             ReadKey();
         }
 
+        /// <summary>
+        /// Форматированный вывод
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="height">Рост</param>
+        /// <param name="historyScore">Баллы по истории</param>
+        /// <param name="mathScore">Баллы по математике</param>
+        /// <param name="rusScore">Баллы по русскому языку</param>
+        /// <param name="averageScore">Средний балл</param>
         static void PrintFormatted(string name, int age, int height, int historyScore, int mathScore, int rusScore, double averageScore)
         {
             WriteLine("\n------------------ Форматированный вывод ----------------------");
@@ -66,6 +95,16 @@ namespace CopyBook
             ReadKey();
         }
 
+        /// <summary>
+        /// Интерполированный вывод
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="height">Рост</param>
+        /// <param name="historyScore">Баллы по истории</param>
+        /// <param name="mathScore">Баллы по математике</param>
+        /// <param name="rusScore">Баллы по русскому языку</param>
+        /// <param name="averageScore">Средний балл</param>
         static void PrintInterpolated(string name, int age, int height, int historyScore, int mathScore, int rusScore, double averageScore)
         {
             WriteLine("\n------------------ Интерполированный вывод ----------------------");
@@ -74,20 +113,39 @@ namespace CopyBook
             ReadKey();
         }
 
+        /// <summary>
+        /// Интерполированный вывод в центре консоли
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="height">Рост</param>
+        /// <param name="historyScore">Баллы по истории</param>
+        /// <param name="mathScore">Баллы по математике</param>
+        /// <param name="rusScore">Баллы по русскому языку</param>
+        /// <param name="averageScore">Средний балл</param>
         static void PrintInterpolatedCentered(string name, int age, int height, int historyScore, int mathScore, int rusScore, double averageScore)
         {
-            int center = WindowWidth / 2;
             WriteLine("\n------------------ Вывод в центре консоли ----------------------");
             string message1 = $"Имя: {name}, возраст: {age}, рост: {height}";
             string message2 = $"Баллы: по истории: {historyScore} по математике: {mathScore} по русскому: {rusScore}";
             string message3 = $"средний балл: {averageScore:0.00}";
-            Console.SetCursorPosition(center, CursorTop);
-            WriteLine(message1);
-            Console.SetCursorPosition(center, CursorTop);
-            WriteLine(message2);
-            Console.SetCursorPosition(center, CursorTop);
-            WriteLine(message3);
+
+            PrintMsgCentered(message1);
+            PrintMsgCentered(message2);
+            PrintMsgCentered(message3);
+
             ReadKey();
+        }
+
+        /// <summary>
+        /// Печатает переданное сообщение в центре консоли
+        /// </summary>
+        /// <param name="msg">Сообщение</param>
+        static void PrintMsgCentered(string msg)
+        {
+            int otstup = (WindowWidth - msg.Length) / 2;
+            SetCursorPosition(otstup, CursorTop);
+            WriteLine(msg);
         }
     }
 }
